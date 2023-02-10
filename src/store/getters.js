2ddce5bff762f23038213563537bf2ad8a2c8f4d
zhuminghong <zhu812114361@qq.com>
2023-02-10 10:47:55 +0800

    first commit

---
+++ src/store/getters.js
@@ -0,0 +1,4 @@
+const getters = {
+  name: state => state.home.name,
+}
+export default getters
---
+++ src/store/modules/home.js
@@ -0,0 +1,11 @@
+const state = {
+  name: '我的名字',
+}
+const mutations = {}
+const actions = {}
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+}
